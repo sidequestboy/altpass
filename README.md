e2c943d75dbe87514812f4261886d28c71c60458
jameh <jamie.alban@gmail.com>
2014-03-12 01:07:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# AltPass
+Generate a password:
+
+```
+$ python altpass.py
+~\1Vow}cF-H`Q^C
+```
+
+The principle here is that the passphrase can be typed on a QWERTY keyboard alternating hands each stroke, which is a nice memory trick, and fun to do.
+
+## Entropy Benchmarks
+The passphrases generated have log2(1092/47)~4.5 bits of entropy per character.
+
+A completely random password on the same character set has log2(94)~6.55 bits per character
+
+A randomly selected English phrase has ~ 1 bit per character ([Shannon 1948][1])
+
+An [xkcd password][1] has ~ 1.6 bits per character
+
+## Randomness
+The randomness is supplied by your operating system's urandom. This ought to be cryptographically secure.
+
+## How many characters should you use?
+Apparently, the state of the art of cracking passwords is 300 billion guesses per second. So, we want our password space to be pretty big. With `n` bits of entropy, if someone were targetting you with this much power, it would take an average of `(300 billion)*2^(n-1)` seconds to crack your password.
+
+In other terms, the probability of them cracking your password after `t` seconds is `(300 billion)*t / 2^n`. We can set this probability arbitrarily low for a given `t` with a sufficiently large number of bits, `n`.
+
+Suppose we require that the probability of cracking before 6 months is less than 1%, then we should have `n = log2((300*10^9)*(6*30*24*60*60)/0.01)~69` bits of entropy in our password. In our algorithm's case, the corresponding number of characters is `c=n/4.5`, so 15 characters is sufficient in this case.
+
+## I can't remember this password!
+It just takes a little practice! However, maybe [xkcd passwords][2] are more your speed (about 3 times as many characters, but actual words!).
+
+[1]: http://www.scribd.com/doc/166218043/Shannon-1948
+[2]: https://github.com/beala/xkcd-password
---
+++ altpass.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Generate passwords that are fun to type"""
+from os import urandom
+
+def choice(keys):
+    """Uniformly choose element from list keys, note len(keys) must be
+    less than 256"""
+    while(True):
+        i = int.from_bytes(urandom(1), byteorder='big')
+        if i < 256 - (256 % len(keys)):
+            return keys[i % len(keys)]
+
+# QWERTY layout:
+left_keys = [["`", "1", "2", "3", "4", "5", "6", ],
+             ["q", "w", "e", "r", "t", ],
+             ["a", "s", "d", "f", "g", ],
+             ["z", "x", "c", "v", ], ]
+left_shift = [["~", "!", "@", "#", "$", "%", "^", ],
+              ["Q", "W", "E", "R", "T", ],
+              ["A", "S", "D", "F", "G", ],
+              ["Z", "X", "C", "V", ], ]
+
+right_keys = [["7", "8", "9", "0", "-", "="],
+              ["y", "u", "i", "o", "p", "[", "]", "\\", ],
+              ["h", "j", "k", "l", ";", "'", ],
+              ["b", "n", "m", ",", ".", "/", ], ]
+right_shift = [["&", "*", "(", ")", "_", "+", ],
+               ["Y", "U", "I", "O", "P", "{", "}", "|", ],
+               ["H", "J", "K", "L", ":", "\"", ],
+               ["B", "N", "M", "<", ">", "?", ], ]
+
+# Alternating hands is fun
+
+# choose randomly which hand to lead
+hand = choice(["left", "right"])
+
+# choose randomly whether to start with shift
+# note that the state of `shift` is a Markov chain with stationary
+# distribution (0.5, 0.5), so draw from the stationary distribution so
+# that the probability of shift being True or False at any given point
+# in the algorithm is 0.5
+shift = choice([True, False])
+
+for i in range(14):
+    if hand == "left":
+        # Choose whether shift key
+        if shift:
+            print(choice([k for l in right_shift for k in l]), end="")
+            shift = False
+            hand = "left"
+        else:
+            print(choice([k for l in left_keys for k in l]), end="")
+            shift = choice([True, False])
+            hand = "right"
+    elif hand == "right":
+        # Choose whether shift key
+        if shift:
+            print(choice([k for l in left_shift for k in l]), end="")
+            shift = False
+            hand = "right"
+        else:
+            print(choice([k for l in right_keys for k in l]), end="")
+            shift = choice([True, False])
+            hand = "left"
+
+print("")
